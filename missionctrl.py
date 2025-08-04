bf29f26e0b00fbd1922c3d0f428b73a507ad3a3a
rsunderr <rshop@sundermeyer.com>
2025-08-03 22:41:52 -0700

    debugged mission control and test fsm

--- missionctrl.py
+++ missionctrl.py
@@ -1,10 +1,5 @@
 from multiprocessing                        import Process, Value
 from shared_memory                          import SharedMemoryWrapper
-from modules.motors.MotorInterface          import MotorInterface
-from modules.pid.pid_interface              import PIDInterface
-from modules.sensors.a50_dvl.dvl_interface  import DVL_Interface
-from modules.vision.vision_main             import VideoRunner
-from utils.kill_button_interface            import Kill_Button_Interface
 from test_fsm                               import Test_FSM
 import time
 """
@@ -18,7 +13,7 @@ import time
 class MissionControl:
     def __init__(self):
         # create shared memory
-        self.shared_memory_object = SharedMemoryWrapper()
+        #self.shared_memory_object = SharedMemoryWrapper()
         self.test_mode = Test_FSM()
         self.test_mode.start()
 
@@ -27,7 +22,7 @@ class MissionControl:
         self.test_mode.join()
 
     def loop(self):
-        while self.shared_memory_object.running.value:
+        while True:#self.shared_memory_object.running.value:
             time.sleep(0.001)
             print(self.state)
             self.test_mode.loop()
