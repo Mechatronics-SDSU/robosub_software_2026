9ec04b4d0074c6b620242556eaa901f63b36e5cc
rsunderr <rshop@sundermeyer.com>
2025-08-04 21:59:13 -0700

    fixed minor issues in mission control

--- missionctrl.py
+++ missionctrl.py
@@ -1,6 +1,7 @@
 from multiprocessing                        import Process, Value
 from shared_memory                          import SharedMemoryWrapper
 from test_fsm                               import Test_FSM
+from spin_fsm                               import Spin_FSM
 import time
 """
     discord: @.kech
@@ -15,15 +16,26 @@ class MissionControl:
         # create shared memory
         self.shared_memory_object = SharedMemoryWrapper()
         self.test_mode = Test_FSM(self.shared_memory_object)
+        self.spin_mode = Spin_FSM(self.shared_memory_object)
+
         self.test_mode.start()
 
         self.loop()
 
         self.test_mode.join()
+        self.spin_mode.join()
 
     def loop(self):
         while True:#self.shared_memory_object.running.value:
             time.sleep(0.001)
-            print(self.state)
+
             self.test_mode.loop()
+            self.spin_mode.loop()
+
+            # transition test mode -> spin mode
+            if self.test_mode.state == "DONE":
+                self.spin_mode.start()
+            # transition spin mode -> off
+            if self.spin_mode.state == "DONE":
+                return
 
