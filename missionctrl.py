6897c678a495919abc8720efb60bd0f1fb5bf9eb
rsunderr <rshop@sundermeyer.com>
2025-08-06 15:32:51 -0700

    fixed mission control shared memory error

--- missionctrl.py
+++ missionctrl.py
@@ -1,7 +1,4 @@
-from multiprocessing                        import Process, Value
 from shared_memory                          import SharedMemoryWrapper
-from test_fsm                               import Test_FSM
-from spin_fsm                               import Spin_FSM
 from gate_fsm                               import Gate_FSM
 from octagon_fsm                            import Octagon_FSM
 import time
@@ -9,34 +6,37 @@ import time
     discord: @.kech
     github: @rsunderr
 
-    Mission Control
+    Mission Control for managing modes
     
 """
-
+# manages modes of operation for the submarine
 class MissionControl:
     def __init__(self):
-        # create shared memory
+        # create shared memory object
         self.shared_memory_object = SharedMemoryWrapper()
-        self.gate_mode  = Gate_FSM()
-        self.oct_mode   = Octagon_FSM()
 
-        self.gate_mode.start()
+        # initialize modes
+        self.gate_mode  = Gate_FSM(self.shared_memory_object)
+        self.oct_mode   = Octagon_FSM(self.shared_memory_object)
+
+        self.gate_mode.start() # start gate mode
     
-        self.loop()
+        self.loop() # loop
 
+        # join processes
         self.gate_mode.join()
         self.oct_mode.join()
 
+    # looping function
     def loop(self):
         while self.shared_memory_object.running.value:
             time.sleep(0.001)
 
             self.gate_mode.loop()
+            self.oct_mode.loop()
 
-            # transition gate mode -> spin mode
-            if self.gate_mode.state == "DONE":
-                self.oct_mode.start()
-            # transition octagon mode -> off
-            if self.oct_mode.state == "DONE":
-                return
+            # transition: gate mode -> octagon mode
+            if self.gate_mode.state == "DONE": self.oct_mode.start()
+            # transition: octagon mode -> off
+            if self.oct_mode.state == "DONE": self.shared_memory_object.running.value = 0 # end loop
 
