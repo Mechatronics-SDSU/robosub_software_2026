3d03ef1c130fd4ec3fe5f409ba082c230c0b6aa6
rsunderr <rshop@sundermeyer.com>
2025-08-06 11:40:32 -0700

    fsms read from yaml for object coords

--- gate_fsm.py
+++ gate_fsm.py
@@ -3,11 +3,12 @@ from shared_memory                          import SharedMemoryWrapper
 from modules.pid.pid_interface              import PIDInterface
 from modules.vision.vision_main             import VideoRunner
 from modules.sensors.a50_dvl.dvl_interface  import DVL_Interface
+import yaml
 """
     discord: @.kech
     github: @rsunderr
 
-    Test FSM
+    Gate FSM
     
 """
 class Gate_FSM:
@@ -15,10 +16,7 @@ class Gate_FSM:
         self.active = False
         # create shared memory
         self.shared_memory_object = shared_memory_object
-        # buffers
-        self.x_buffer = 0.3#m
-        self.y_buffer = 0.3#m
-        self.z_buffer = 0.5#m
+
         # initial state (INIT, DRIVE, DONE)
         self.state = "INIT"
 
@@ -31,6 +29,20 @@ class Gate_FSM:
         self.PID_process = Process(target=self.PID_interface.run_loop)
         self.dvl_process = Process(target=self.dvl_object.run_loop)
         self.vis_process = Process(target=self.vis_object.run_loop)
+
+        # buffers
+        self.x_buffer = 0.3#m
+        self.y_buffer = 0.3#m
+        self.z_buffer = 0.5#m
+
+        # target values
+        self.x1, self.y1, self.z1 = None
+        with open("~/robosub_software_2025/objects.yaml", 'r') as file:
+            data = yaml.safe_load(file)
+            gate = data['objects']['gate']
+            self.x1 = gate['x']
+            self.y1 = gate['y']
+            self.z1 = gate['z']
 
     # start FSM
     def start(self):
@@ -49,9 +61,9 @@ class Gate_FSM:
         if self.state == next: return # do nothing if no state change
         match(next):
             case "DRIVE":
-                self.shared_memory_object.target_x.value = 3#m TODO make us read from a file
-                self.shared_memory_object.target_y.value = 0#m
-                self.shared_memory_object.target_z.value = 1#m
+                self.shared_memory_object.target_x.value = self.x1
+                self.shared_memory_object.target_y.value = self.y1
+                self.shared_memory_object.target_z.value = self.z1
             case "DONE":
                 print("DONE")
                 self.stop()
@@ -68,7 +80,7 @@ class Gate_FSM:
         # transitions
         match(self.state):
             case "DRIVE":
-                if abs(self.shared_memory_object.dvl_x.value - self.shared_memory_object.target_x.vlaue) <= self.x_buffer and abs(self.shared_memory_object.dvl_y.value - self.shared_memory_object.target_y.value) <= self.y_buffer and abs(self.shared_memory_object.dvl_z.value - self.shared_memory_object.target_z.value) <= self.z_buffer:
+                if abs(self.shared_memory_object.dvl_x.value - self.x1) <= self.x_buffer and abs(self.shared_memory_object.dvl_y.value - self.y1) <= self.y_buffer and abs(self.shared_memory_object.dvl_z.value - self.self.z1) <= self.z_buffer:
                     next = "DONE"
             case _: # do nothing if invalid state
                 print("invalid state")
