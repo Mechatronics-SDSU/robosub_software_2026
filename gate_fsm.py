cca94f8f9ab53dafb617e430c6a4746a8f20fe49
rsunderr <rshop@sundermeyer.com>
2025-08-08 16:00:28 -0700

    fixed issues in mission control code

--- gate_fsm.py
+++ gate_fsm.py
@@ -13,13 +13,19 @@ import os
     FSM for navigating through gate
     
 """
-# FSM for gate mode
+
 class Gate_FSM:
+    """
+    FSM for gate mode - driving through the gate
+    """
     def __init__(self, shared_memory_object):
+        """
+        Gate FSM constructor
+        """
         # create shared memory
         self.shared_memory_object = shared_memory_object
 
-        # initial state (INIT, DRIVE, DONE)
+        # initial state (INIT, DRIVE, NEXT/DONE)
         self.state = "INIT"
         self.active = False
 
@@ -35,10 +41,10 @@ class Gate_FSM:
 
         # buffers
         self.x_buffer = 0.3#m
-        self.y_buffer = 10#m
-        self.z_buffer = 10#m
+        self.y_buffer = 0.3#m
+        self.z_buffer = 0.5#m
 
-        # target values
+        # TARGET VALUES-----------------------------------------------------------------------------------------------------------------------
         self.gate_x, self.gate_y, self.gate_z = (None, None, None)
         with open(os.path.expanduser("~/robosub_software_2025/objects.yaml"), 'r') as file: # read from yaml
             data = yaml.safe_load(file)
@@ -46,35 +52,39 @@ class Gate_FSM:
             self.gate_y = data['objects']['gate']['y']
             self.gate_z = data['objects']['gate']['z']
 
-    # start FSM
     def start(self):
+        """
+        Start FSM
+        """
         self.active = True
+        print("STARTING GATE MODE")
 
         # start processes
         self.PID_process.start()
         self.dvl_process.start()
         #self.vis_process.start()
 
-        #set_screen((int(self.shared_memory_object.dvl_x.value), int(self.shared_memory_object.dvl_y.value), int(self.shared_memory_object.dvl_z.value)), "GATE:DRIVE", "yippeeeee")
-
         # set initial state
         self.next_state("DRIVE")
 
-    # change to next state
     def next_state(self, next):
+        """
+        Change to next state
+        """
         if not self.active or self.state == next: return # do nothing if not enabled or no state change
+        # STATES-----------------------------------------------------------------------------------------------------------------------
         match(next):
-            case "INIT":
-                pass
-            case "DRIVE":
+            case "INIT": pass # initial state
+            case "DRIVE": # drive toward gate
+                print("DRIVE")
                 self.shared_memory_object.target_x.value = self.gate_x
                 self.shared_memory_object.target_y.value = self.gate_y
                 self.shared_memory_object.target_z.value = self.gate_z
-            case "NEXT":
+            case "NEXT": # disable but not kill (go to next mode)
                 print("NEXT")
                 self.active = False
                 return
-            case "DONE":
+            case "DONE": # fully disable and kill
                 print("DONE")
                 self.stop()
                 return
@@ -83,37 +93,49 @@ class Gate_FSM:
                 return
         self.state = next
 
-    # loop function (mostly transitions)
     def loop(self):
+        """
+        Loop function, mostly state transitions within conditionals
+        """
         if not self.active: return # do nothing if not enabled
-        # transitions
+        # display
+        #set_screen((0, 255, 0), "GATE:DRIVE", "0, 0, 0")
+
+        # TRANSITIONS------------------------------------------------------------------------------------------------------
         match(self.state):
-            case "INIT":
-                pass
-            case "DRIVE": # transition: DRIVE -> DONE
+            case "INIT": pass
+            case "DRIVE": # transition: DRIVE -> NEXT
                 if self.reached_xyz(self.gate_x, self.gate_y, self.gate_z):
                     self.next_state("NEXT")
+            case "NEXT": pass
+            case "DONE": pass
             case _: # do nothing if invalid state
                 print("GATE: INVALID LOOP STATE", self.state)
                 return
     
-    # returns true if near a location (requires x,y,z buffer and dvl to work)
     def reached_xyz(self, x, y, z):
+        """
+        Returns true if near a location (requires x,y,z buffer and dvl to work)
+        """
         if abs(self.shared_memory_object.dvl_x.value - x) <= self.x_buffer and abs(self.shared_memory_object.dvl_y.value - y) <= self.y_buffer and abs(self.shared_memory_object.dvl_z.value - z) <= self.z_buffer:
             return True
         # else
         return False
 
-    # wait until child processes terminate
     def join(self):
+        """
+        Wait until child processes terminate
+        """
         if not self.active: return # do nothing if not enabled
         # join processes
         self.PID_process.join()
         self.dvl_process.join()
         #self.vis_process.join()
 
-    # stop FSM
     def stop(self):
+        """
+        Stop FSM
+        """
         self.active = False
         # terminate processes
         self.PID_process.terminate()
