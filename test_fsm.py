c792b572c51bdc38337d3a713217d9199a8d914e
rsunderr <rshop@sundermeyer.com>
2025-08-08 20:45:42 -0700

    minor fixes and improvements to comments

--- test_fsm.py
+++ test_fsm.py
@@ -6,7 +6,7 @@ import os
     discord: @.kech
     github: @rsunderr
 
-    FSM for navigating through gate
+    FSM for testing purposes
     
 """
 
@@ -52,29 +52,23 @@ class Test_FSM(FSM_Template):
         # STATES-----------------------------------------------------------------------------------------------------------------------
         match(next):
             case "INIT": pass # initial state
-            case "DRIVE": # drive toward gate
+            case "DRIVE": 
                 print("DRIVE")
                 self.shared_memory_object.target_x.value = self.gate_x
                 self.shared_memory_object.target_y.value = self.gate_y
                 self.shared_memory_object.target_z.value = self.gate_z
-            case "NEXT": # disable but not kill (go to next mode)
+            case "NEXT": 
                 print("NEXT")
                 self.shared_memory_object.target_x.value = 2
                 self.shared_memory_object.target_y.value = 0
                 self.shared_memory_object.target_z.value = 0
             case "DONE": # fully disable and kill
-                set_screen(
-                    (0, 255, 0),
-                    f"GATE:{self.state}",
-                    ""
-                )
+                self.display(255, 0, 0)
                 print("DONE")
                 self.active = False
                 self.stop()
-                
-                return
             case _: # do nothing if invalid state
-                print("GATE: INVALID NEXT STATE", self.state)
+                print("TEST: INVALID NEXT STATE", self.state)
                 return
         self.state = next
 
@@ -83,12 +77,7 @@ class Test_FSM(FSM_Template):
         Loop function, mostly state transitions within conditionals
         """
         if not self.active: return # do nothing if not enabled
-        # display
-        set_screen(
-            (0, 255, 0),
-            f"GATE:{self.state}",
-            f"Target: | DVL: \nx={self.shared_memory_object.dvl_x.value} y={self.shared_memory_object.dvl_y.value}. z={self.shared_memory_object.dvl_z.value}\n t_x={self.shared_memory_object.target_x.value}. t_y={self.shared_memory_object.target_y.value}. t_z={self.shared_memory_object.target_z.value}"
-        )
+        self.display(255,0,0) # update display
 
         # TRANSITIONS------------------------------------------------------------------------------------------------------
         match(self.state):
