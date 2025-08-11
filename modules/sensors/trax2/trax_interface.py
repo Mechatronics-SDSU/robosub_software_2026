2bf48326895afa7ea3f4f940dbf3408b6cc6009a
rsunderr <rshop@sundermeyer.com>
2025-08-11 09:47:08 -0700

    fixed continuous data issues

--- modules/sensors/trax2/trax_interface.py
+++ modules/sensors/trax2/trax_interface.py
@@ -21,7 +21,6 @@ class Trax_Interface:
         self.t_prev = time.time()
         self.continuous = False
         self.payload = None
-        self.data = []
 
         # positional data
         self.trax_x = 0 # eventually put these in shared memory
@@ -37,36 +36,18 @@ class Trax_Interface:
         self.trax_roll  = 0
 
         # kSetAcqParams
-        frameID = "kSetAcqParams" # OR =24
-        self.payload = (False, False, 0.0, 0.001)
-        self.trax.send_packet(frameID, self.payload)
+        frameID = "kSetAcqParams"
+        payload = (False, False, 0.0, 0.05) # T/F poll mode/cont mode, T/F compass FIR mode, PNI reserved, delay
+        self.trax.send_packet(frameID, payload)
 
-        # kSetAcqParamsDone
-        #self.data = self.trax.recv_packet()
-        #print("kSetAcqParamsDone:", self.data[1] == 26)
+        # kSetAcqParamsDone (optional)
+        #data = self.trax.recv_packet()
+        #print("kSetAcqParamsDone:", data[1] == 26)
 
         # kSetDataComponents
         frameID = "kSetDataComponents"
         self.payload = (6, 0x15, 0x16, 0x17, 0x5, 0x18, 0x19) # 6 comp's: ax ay az yaw pitch roll
         self.trax.send_packet(frameID, self.payload)
-
-    def update(self):
-        """
-        message_type, dvl_data = self.dvl.recieveData()
-        if dvl_data != None and message_type == "dead_reckoning":
-            self.shared_memory_object.dvl_yaw.value = dvl_data[0]
-            self.shared_memory_object.dvl_pitch.value = dvl_data[1]
-            self.shared_memory_object.dvl_roll.value = dvl_data[2]
-            self.shared_memory_object.dvl_x.value = -dvl_data[3]
-            self.shared_memory_object.dvl_y.value = dvl_data[4]
-            self.shared_memory_object.dvl_z.value = dvl_data[5]
-            print("dvl_yaw:", self.shared_memory_object.dvl_yaw.value)
-            print("dvl_pitch:", self.shared_memory_object.dvl_pitch.value)
-            print("dvl_roll:", self.shared_memory_object.dvl_roll.value)
-            print("dvl_x:", self.shared_memory_object.dvl_x.value)
-            print("dvl_y:", self.shared_memory_object.dvl_y.value)
-            print("dvl_z:", self.shared_memory_object.dvl_z.value)
-        """
 
     def run_loop(self):
         """
@@ -74,26 +55,31 @@ class Trax_Interface:
         """
         # start continuous mode if not already started
         if not self.continuous:
-            #self.start_cont_mode()
+            self.stop_cont_mode()
+            self.start_cont_mode()
             self.continuous = True
         # get data
-        self.data = self.trax.recv_packet(self.payload)
-        print(self.data)
+        data = self.trax.recv_packet(self.payload)
+        """
         # update values
-        self.trax_yaw = self.data[3]
-        self.trax_pitch = self.data[4]
-        self.trax_roll = self.data[5]
         t = time.time()
         if t - self.t_prev == 0.05: # run only every 0.05s
-            self.vel_x += self.data[0]
-            self.vel_y += self.data[1]
-            self.vel_z += self.data[2]
+            self.trax_yaw = yaw
+            self.trax_pitch = pitch
+            self.trax_roll = roll
+
+            self.vel_x += ax
+            self.vel_y += ay
+            self.vel_z += az
 
             self.trax_x += self.vel_x
             self.trax_y += self.vel_y
             self.trax_z += self.vel_z
 
             self.t_prev = t
+        
+        print(f"ax={ax} ay={ay} az={az}\nvx={self.vel_x} vy={self.vy} vz={self.vel_z}\nx={self.trax_x} y={self.trax_y} z={self.trax_z}\nyaw={yaw} pitch={pitch} roll={roll}")
+        """
 
     def start_cont_mode(self):
         """
@@ -112,6 +98,17 @@ class Trax_Interface:
         # kStopContinuousMode
         frameID = "kStopContinuousMode"
         self.trax.send_packet(frameID)
+    
+    def get_data(self):# 6 comp's: ax ay az yaw pitch roll
+        data = self.trax.recv_packet(self.payload)
+        print(data)
+        ax = data[4]
+        ay = data[6]
+        az = data[8]
+        yaw = data[10]
+        pitch = data[12]
+        roll = data[14]
+        return (ax, ay, az, yaw, pitch, roll)
 
 if __name__ == "__main__":
     shared_memory_object = "placeholder"
