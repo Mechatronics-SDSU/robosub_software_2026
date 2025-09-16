cc46831b29e1c3afefdd13bf889e471446974286
rsunderr <rshop@sundermeyer.com>
2025-09-15 20:00:17 -0700

    prepped for intro project

--- modules/motors/MotorWrapper.py
+++ modules/motors/MotorWrapper.py
@@ -1,33 +1,17 @@
 import numpy as np
-from shared_memory import SharedMemoryWrapper
-try:
-    from modules.motors.USB_Transmit import USB_Transmitter
-except:
-    from USB_Transmit import USB_Transmitter
-"""
-usbData[0] = motor0; 
-usbData[1] = motor1; 
-usbData[2] = motor2; 
-usbData[3] = motor3; 
-usbData[4] = motor4; 
-usbData[5] = motor5;
-usbData[6] = motor6; 
-usbData[7] = motor7; 
-usbData[8] = killState; 
-usbData[9] = powerOffState; 
-usbData[10] = red; 
-usbData[11] = green; 
-usbData[12] = blue;
-"""
+import time
 
 class MotorWrapper:
 
-    def __init__(self, shared_memory_object):    
-        self.shared_memory_object = shared_memory_object
-        self.usb_transmitter = USB_Transmitter()
+    def __init__(self): #, shared_memory_object): #(REMOVED FOR INTRO PROJECT)
+        """
+        MotorWrapper constructor
+        """
+        #self.shared_memory_object = shared_memory_object #(REMOVED FOR INTRO PROJECT)
+        #self.usb_transmitter = USB_Transmitter() #(REMOVED FOR INTRO PROJECT)
         #-------------------------------------------------------------------------------------------------
         self.MOTOR_MAX    = 4000
-        self.MOTOR_FACTOR = 0.7 # [0.0, 1.0] -- set ~0.1 for in air, ~0.3 in water
+        self.MOTOR_FACTOR = 1.0 # [0.0, 1.0] -- set ~0.1 for in air, ~0.3 in water
         #-------------------------------------------------------------------------------------------------
 
         self.motors = np.array([
@@ -46,13 +30,19 @@ class MotorWrapper:
         self.controls   = [0, 0, 0, 255, 0] # control values (kill, power off, lights R,G,B) FIXME assign to shared mem vals
         self.motor_vals = [0, 0, 0, 0, 0, 0, 0, 0] # motor values
 
-    # returns a validated version of the motor value
     def valid(self, motor_val):
+        """
+        Returns a validated version of the motor value
+        """
         motor_val = int(motor_val)
         if type(motor_val) != int and type(motor_val) != float: return 0 # return 0 if not a number
         # multiply clamped motor value by motor factor, cast to int
         return int(self.MOTOR_FACTOR * np.clip(motor_val, -self.MOTOR_MAX, self.MOTOR_MAX))
 
+    
+    """
+    Movement functions expect an single integer for speed as parameter, range [-MOTOR_MAX to MOTOR_MAX]
+    """
     def move_forward(self, value):
         self.move_from_matrix(np.array([self.valid(value), 0, 0, 0, 0, 0]))
 
@@ -97,22 +87,33 @@ class MotorWrapper:
 
 
     def move_from_matrix(self, matrix):
-        #translate the direction vector matrix to motor values
-        # print(matrix)
+        """
+        Translates the direction vector matrix to motor values
+        """
+        #print(matrix)
         temp_list = np.round(np.dot(matrix, self.motors.transpose()))
         self.motor_vals += temp_list
-        # print(temp_list)
+        #print(temp_list)
 
-    #sends commands to motors
     def send_command(self):
+        """
+        Sends commands to motors
+        """
         send_data = np.concatenate((self.motor_vals, self.controls), axis=None).astype(int)
         for i, data in enumerate(send_data):
             send_data[i] = self.valid(data)
-        # print(send_data)
-        self.usb_transmitter.send_data(list(send_data)) # concatenate motor and control values
+        print(send_data)
+        #self.usb_transmitter.send_data(list(send_data)) # concatenate motor and control values (REMOVED FOR INTRO PROJECT)
 
         motor_values = self.motor_vals # save motor values
-        #self.shared_memory_object.motor_values.value = motor_values # save to shared memory
+        #self.shared_memory_object.motor_values.value = motor_values # save to shared memory (REMOVED FOR INTRO PROJECT)
         self.stop() # reset motor values to 0s
 
         return motor_values # return motor values
+    
+if __name__ == "__main__":
+    """
+    Your Code Here
+    Just run the file to test
+    
+    """
