a4fe847fb34877d350428a8326aa6a8bcac7fcbf
rsunderr <rshop@sundermeyer.com>
2025-10-13 20:31:52 -0700

    fsms using enums for matches instead of strs, removed redundant DONE states, return no longer stops itself (relying on next() fxn in fsm.py to call stop when next mode is None), various minor improvements to fsms, improved formatting in test_msn_control

--- fsm/slalom_fsm.py
+++ fsm/slalom_fsm.py
@@ -1,5 +1,6 @@
 from utils.socket_send                      import set_screen
 from fsm.fsm                                import FSM_Template
+from enum                                   import Enum
 import yaml, os
 """
     discord: @.kech
@@ -8,6 +9,17 @@ import yaml, os
     FSM for navigating through gate
     
 """
+class States(Enum):
+    """
+    Enumeration for FSM states
+    """
+    INIT    = "INIT"
+    TO_START= "TO_START"
+    TO_MID  = "TO_MID"
+    TO_END  = "TO_END"
+    
+    def __str__(self) -> str: # make elegant string
+        return self.value
 
 class Slalom_FSM(FSM_Template):
     """
@@ -20,9 +32,10 @@ class Slalom_FSM(FSM_Template):
         # call parent constructor
         super().__init__(shared_memory_object, run_list)
         self.name = "SLALOM"
+        self.state = States.INIT  # initial state
 
         # TARGET VALUES-----------------------------------------------------------------------------------------------------------------------
-        self.x_buffer = self.y_buffer = self.z_buffer = self.x1 = self.y1 = self.x2 = self.y2 = self.x3 = self.y3 = self.depth = 0
+        self.x1 = self.y1 = self.x2 = self.y2 = self.x3 = self.y3 = self.depth = 0
         try:
             with open(os.path.expanduser("~/robosub_software_2025/objects.yaml"), 'r') as file: # read from yaml
                 data = yaml.safe_load(file)
@@ -47,28 +60,26 @@ class Slalom_FSM(FSM_Template):
         super().start()  # call parent start method
 
         # set initial state
-        self.next_state("TO_START")
+        self.next_state(States.TO_START)
 
-    def next_state(self, next: str) -> None:
+    def next_state(self, next: States) -> None:
         """
         Change to next state
         """
         if not self.active or self.state == next: return # do nothing if not enabled or no state change
         # STATES-----------------------------------------------------------------------------------------------------------------------
         match(next):
-            case "INIT": return # initial state
-            case "TO_START": # drive to start of slalom
+            case States.INIT: return # initial state
+            case States.TO_START: # drive to start of slalom
                 self.shared_memory_object.target_x.value = self.x1
                 self.shared_memory_object.target_y.value = self.y1
                 self.shared_memory_object.target_z.value = self.depth
-            case "TO_MID": # drive to middle of slalom
+            case States.TO_MID: # drive to middle of slalom
                 self.shared_memory_object.target_x.value = self.x2
                 self.shared_memory_object.target_y.value = self.y2
-            case "TO_END": # drive to end of slalom
+            case States.TO_END: # drive to end of slalom
                 self.shared_memory_object.target_x.value = self.x3
                 self.shared_memory_object.target_y.value = self.y3
-            case "DONE": # disable but not kill (go to next mode)
-                self.suspend()
             case _: # do nothing if invalid state
                 print(f"{self.name} INVALID NEXT STATE {next}")
                 return
@@ -84,16 +95,16 @@ class Slalom_FSM(FSM_Template):
 
         # TRANSITIONS------------------------------------------------------------------------------------------------------
         match(self.state):
-            case "INIT" | "DONE": return
-            case "TO_START": # transition: TO_START -> TO_MID
+            case States.INIT: return
+            case States.TO_START: # transition: TO_START -> TO_MID
                 if self.reached_xyz(self.x1, self.y1, self.depth):
-                    self.next_state("TO_MID")
-            case "TO_MID": # transition: TO_MID -> TO_END
+                    self.next_state(States.TO_MID)
+            case States.TO_MID: # transition: TO_MID -> TO_END
                 if self.reached_xyz(self.x2, self.y2, self.depth):
-                    self.next_state("TO_END")
-            case "TO_END": # transition: TO_END -> DONE
+                    self.next_state(States.TO_END)
+            case States.TO_END: # transition: TO_END -> DONE
                 if self.reached_xyz(self.x3, self.y3, self.depth):
-                    self.next_state("DONE")
+                    self.suspend()
             case _: # do nothing if invalid state
                 print(f"{self.name} INVALID STATE {self.state}")
 
