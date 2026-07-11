1c61e1fecb8b45a957c5c398f035132a6f6b1d64
alicvo <141070918+alicvo@users.noreply.github.com>
2026-07-11 11:20:29 -0700

    use environ variables for k values

--- modules/pid/pid_interface.py
+++ modules/pid/pid_interface.py
@@ -1,3 +1,15 @@
+import os
+
+import numpy as np
+import time
+from modules.pid.six_dof_pid import PID
+from scipy.spatial.transform import Rotation as R
+
+try: 
+    from modules.motors.ScionMotorWrapper       import MotorWrapper # type: ignore
+except ImportError:
+    from modules.motors.MotorWrapper            import MotorWrapper
+
 """
     discord: @kialli
     github: @kchan5071
@@ -5,54 +17,58 @@
     PID methods, integration (orientation code)
 
 """
-
-import numpy as np
-import time
-from modules.pid.six_dof_pid import PID
-from scipy.spatial.transform import Rotation as R
-
-try:
-    from modules.motors.ScionMotorWrapper       import MotorWrapper # type: ignore
-except ImportError:
-    from modules.motors.MotorWrapper            import MotorWrapper
-
 P_DEBUG = False
-TIME_SLEEP = 0.2
 
 class PIDInterface:
     def __init__(self, shared_memory_object):
         self.shared_memory_object = shared_memory_object
         self.motor_wrapper = MotorWrapper(self.shared_memory_object)
 
+        #SIMULATION
+        # self.simulation = Simulation(np.array([0, 0, 0, 0, 0, 0], dtype=float))
+
         # array of PID k Values
-        self.K_array = np.array([
-        #     x,     y,     z,     yaw,  pitch, roll
-            [3000,  3000,  2500,    30,    2,    2], #kp
-            [.5,   .5,      2,      2,    .5,   .5], #ki
-            [.1,   .1 ,    .1,     .2,    .5,   .5]  #kd
-        ])
-        self.pid_object = PID(self.K_array[0], self.K_array[1], self.K_array[2], 0.5)
+        """ 
+        #                          x,      y,     z,    yaw,   pitch, roll
+        self.K_array = np.array([[3000,  3000,  2500,    30,     2,    2], #kp
+                                 [.5,   .5,      2,      2,    .5,   .5],  #ki
+                                 [.1,   .1 ,    .1,     .2,    .5,   .5]]) #kd
+        """
         
+        kp_row = self.get_env_array("KP_ROW", [3000, 3000, 2500, 30, 2, 2])
+        ki_row = self.get_env_array("KI_ROW", [0.5, 0.5, 2, 2, 0.5, 0.5])
+        kd_row = self.get_env_array("KD_ROW", [0.1, 0.1, 0.1, 0.2, 0.5, 0.5])
+
+        self.K_array = np.array([
+            kp_row,
+            ki_row,
+            kd_row
+        ])
+
+    def get_env_array(self, name: str, default: list[float]) -> np.ndarray:
+        value = os.environ.get(name)
+
+        if value is None:
+            return np.array(default, dtype=float)
+
+        return np.array([float(x.strip()) for x in value.split(",")], dtype=float)
     
     def run_pid(self):
-        desired_state = np.array([
-            self.shared_memory_object.target_x.value,
-            self.shared_memory_object.target_y.value,
-            self.shared_memory_object.target_z.value,
-            self.shared_memory_object.target_yaw.value,
-            self.shared_memory_object.target_pitch.value,
-            self.shared_memory_object.target_roll.value
-        ])
-        current_state = np.array([
-            self.shared_memory_object.dvl_x.value,
-            self.shared_memory_object.dvl_y.value,
-            self.shared_memory_object.dvl_z.value,
-            self.shared_memory_object.dvl_yaw.value,
-            self.shared_memory_object.dvl_pitch.value,
-            self.shared_memory_object.dvl_roll.value
-        ])
+        desired_state = np.array([self.shared_memory_object.target_x.value, 
+                                  self.shared_memory_object.target_y.value, 
+                                  self.shared_memory_object.target_z.value, 
+                                  self.shared_memory_object.target_yaw.value, 
+                                  self.shared_memory_object.target_pitch.value, 
+                                  self.shared_memory_object.target_roll.value])
+        current_state = np.array([self.shared_memory_object.dvl_x.value, 
+                                  self.shared_memory_object.dvl_y.value, 
+                                  self.shared_memory_object.dvl_z.value, 
+                                  self.shared_memory_object.dvl_yaw.value, 
+                                  self.shared_memory_object.dvl_pitch.value, 
+                                  self.shared_memory_object.dvl_roll.value])
+        self.pid = PID(self.K_array[0], self.K_array[1], self.K_array[2], 0.5)
         # Get the PID output: movement in robot's local frame
-        untransformed = self.pid_object.update(current_state, desired_state)  # [x, y, z, yaw, pitch, roll]
+        untransformed = self.pid.update(current_state, desired_state)  # [x, y, z, yaw, pitch, roll]
 
         # Split into linear and angular components
         movement_local = np.array(untransformed[:3])  # Linear: x, y, z (local frame)
@@ -69,54 +85,36 @@ class PIDInterface:
 
         # Combine with angular commands (still in local frame)
         movement_global = np.concatenate([movement_global_linear, angular])
-
-        # this is needed to fix the direction fo the motors on the actual sub
-        movement_global = (np.multiply(movement_global, [1,-1,-1,-1,1,1]))
-
         if P_DEBUG:
             print("Untransformed: ", untransformed)
             print("Yaw, Pitch, Roll: ", (yaw, pitch, roll))
             print("Transformed: ",movement_global)
-        
+        movement_global = (np.multiply(movement_global, [1,-1,-1,-1,1,1]))
         return movement_global, untransformed
     
     def run_loop(self):
         np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-
-        # run loop while the sub is running
         while self.shared_memory_object.running.value:
-            # get pid valeus
             direction, untransformed_direction = self.run_pid()
-
-            # set motor matrix and send command to motor hardware
             self.motor_wrapper.move_from_matrix(direction)
             self.motor_wrapper.send_command()
-
-            # print debug for errors
+            error = np.subtract(np.array([self.shared_memory_object.target_x.value, 
+                                          self.shared_memory_object.target_y.value, 
+                                          self.shared_memory_object.target_z.value, 
+                                          self.shared_memory_object.target_yaw.value, 
+                                          self.shared_memory_object.target_pitch.value, 
+                                          self.shared_memory_object.target_roll.value]), 
+                                np.array([self.shared_memory_object.dvl_x.value, 
+                                          self.shared_memory_object.dvl_y.value, 
+                                          self.shared_memory_object.dvl_z.value, 
+                                          self.shared_memory_object.dvl_yaw.value, 
+                                          self.shared_memory_object.dvl_pitch.value, 
+                                          self.shared_memory_object.dvl_roll.value]))
             if P_DEBUG:
-                # calculate error
-                error = np.subtract(
-                    np.array([
-                        self.shared_memory_object.target_x.value,
-                        self.shared_memory_object.target_y.value,
-                        self.shared_memory_object.target_z.value,
-                        self.shared_memory_object.target_yaw.value,
-                        self.shared_memory_object.target_pitch.value,
-                        self.shared_memory_object.target_roll.value
-                    ]),
-                    np.array([
-                        self.shared_memory_object.dvl_x.value,
-                        self.shared_memory_object.dvl_y.value,
-                        self.shared_memory_object.dvl_z.value,
-                        self.shared_memory_object.dvl_yaw.value,
-                        self.shared_memory_object.dvl_pitch.value,
-                        self.shared_memory_object.dvl_roll.value
-                    ]))
-                
                 print("Direction: ", direction)
                 print("Untransformed Direction: ", untransformed_direction)
                 print("Linear Error: ", np.sum(error[:3]))
                 print("Angular Error: ", np.sum(error[3:]))
 
             
-            time.sleep(TIME_SLEEP)
+            time.sleep(0.2)
